903d3a3bf438df8a8f568df3341bc488d7d906da
jalal <jlalalhmaty7@gmail.com>
2026-09-19 01:34:11 +0300

    P02 rev: remove obsolete theme import

--- mobile_app/lib/features/support/presentation/p02_support_info_screen.dart
+++ mobile_app/lib/features/support/presentation/p02_support_info_screen.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
 
-import '../../../core/theme/app_theme.dart';
 import '../../../core/widgets/app_components.dart';
 import '../../account/data/auth_controller.dart';
 
